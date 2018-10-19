146027d95a9ece5f43031b43d374a8cf2ab0f307
echavez85 <echavez85@gmail.com>
2018-10-19 18:17:40 -0500

    Created JS pseudocode.

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// List of word choices (Drag Names)
+
+
+// How to start the game (Click or press any key)
+
+
+// Randomly choose a name from list and display only underscores based on length of the name.
+
+
+// Player inputs guesses.
+
+
+// Correct guesses should be displayed in place of underscores above.
+
+
+// Wrong guesses will be put in a separate section. 
+
+
+// Each wrong guess will decrease the number of guesses left.
+
+
+// If the full name is guessed and guesses are greater than 0, new random word is chosen for player.
+
+
+// If guesses left equals 0, display game over and allow player to choose to restart.
